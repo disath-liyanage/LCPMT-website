294a5ac3274370e3a8453e855c7d9f26b181d0e5
Disath Liyanage <disathsenitha@gmail.com>
2026-08-04 12:27:01 +0530

    projects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
   plugins: [
     require("tailwindcss-animate"),
     require("@tailwindcss/typography"),
   ],
-}
+};
+export default config;
